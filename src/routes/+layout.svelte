09e3ac37c6dd3d806b11ac9ca8cea4388492d341
demmyhonore <demmyhonore@gmail.com>
2023-10-10 17:03:38 +0900

    Add html and css reset

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,42 @@
+<slot />
+
+<style>
+	/* Variables */
+	:global(:root) {
+		--random-color: blue;
+	}
+
+	/* Reset by https://www.joshwcomeau.com/css/custom-css-reset/ */
+	:global(*, *::before, *::after) {
+		box-sizing: border-box;
+	}
+
+	:global(*) {
+		margin: 0;
+	}
+
+	:global(body) {
+		line-height: 1.5;
+		-webkit-font-smoothing: antialiased;
+	}
+
+	:global(img, picture, video, canvas, svg) {
+		display: block;
+		max-width: 100%;
+	}
+
+	:global(input, button, textarea, select) {
+		font: inherit;
+	}
+
+	:global(p, h1, h2, h3, h4, h5, h6) {
+		overflow-wrap: break-word;
+	}
+
+	/* Body as container */
+	:global(body) {
+		display: grid;
+		place-items: center;
+		height: 100vh;
+	}
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,37 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<main>
+	<article>
+		<h1>Password Generator</h1>
+		<div>
+			<p>P4$5W0rD!</p>
+			<button>Copy</button>
+		</div>
+		<form>
+			<fieldset>
+				<legend />
+				<div>
+					<label for="character-length">Character Length 10</label>
+					<input type="range" id="character-length" name="character-length" min="0" max="11" />
+				</div>
+				<div>
+					<input type="checkbox" id="include-uppercase" name="include-uppercase" checked />
+					<label for="include-uppercase">Include Uppercase Letters</label>
+				</div>
+
+				<div>
+					<input type="checkbox" id="include-lowercase" name="include-lowercase" />
+					<label for="include-lowercase">Include Lowercase Letters</label>
+				</div>
+				<div>
+					<input type="checkbox" id="include-numbers" name="include-numbers" />
+					<label for="include-numbers"> Include Numbers</label>
+				</div>
+				<div>
+					<input type="checkbox" id="include-symbols" name="include-symbols" />
+					<label for="include-symbols"> Include Symbols</label>
+				</div>
+			</fieldset>
+			<p>Strength medium</p>
+			<button>Generate</button>
+		</form>
+	</article>
+</main>
