972895bfdb8e094d9b09f0753bfe74ae88bd994e
demmyhonore <demmyhonore@gmail.com>
2023-10-10 23:07:40 +0900

    Add font, vars, and body css

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,12 +1,27 @@
 <slot />
 
 <style>
-	/* Variables */
-	:global(:root) {
-		--random-color: blue;
+	/* Font */
+	@font-face {
+		font-family: 'JetBrains Mono';
+		src: url('/fonts/JetBrainsMono-VariableFont_wght.ttf') format('truetype');
+		font-weight: 100 1000;
 	}
 
-	/* Reset by https://www.joshwcomeau.com/css/custom-css-reset/ */
+	/* Variables */
+	:global(:root) {
+		--clr-almost-white: hsl(252, 11%, 91%);
+		--clr-dark-grey: hsl(248, 10%, 15%);
+		--clr-grey: hsl(251, 9%, 53%);
+		--clr-neon-green: hsl(127, 100%, 82%);
+		--clr-orange: hsl(13, 95%, 66%);
+		--clr-red: hsl(0, 91%, 63%);
+		--clr-very-dark-grey: hsl(248, 15%, 11%);
+		--clr-yellow: hsl(42, 91%, 68%);
+		--ff-base: 'JetBrains Mono', monospace;
+	}
+
+	/* Reset */
 	:global(*, *::before, *::after) {
 		box-sizing: border-box;
 	}
@@ -33,10 +48,14 @@
 		overflow-wrap: break-word;
 	}
 
-	/* Body as container */
+	/* Global */
 	:global(body) {
+		height: 100vh;
+		background-color: var(--clr-very-dark-grey);
+		font-family: var(--ff-base);
+		font-size: 1.125rem;
+		color: var(--clr-almost-white);
 		display: grid;
 		place-items: center;
-		height: 100vh;
 	}
 </style>
